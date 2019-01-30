098b5b3430c402bbae600edb7abee60ee8f146ff
Vicens Noguera David (ERNI) <noda@erni-espana.es>
2019-01-30 10:32:12 +0100

    Initial commit

---
+++ src/main/java/com/users/crud/UsersBackApplication.java
@@ -0,0 +1,14 @@
+package com.users.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersBackApplication.class, args);
+	}
+
+}
+
